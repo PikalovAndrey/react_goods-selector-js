9ec66b5a9fe377792dbad31de29efbdc0a087d5e
PikalovAndrey <andrtan10mail.com@gmail.com>
2024-09-16 12:21:22 +0300

    improve the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import 'bulma/css/bulma.css';
+import React from 'react';
 import './App.scss';
 import { useState } from 'react';
+import classNames from 'classnames';
+
 
 export const goods = [
   'Dumplings',
@@ -16,46 +19,44 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [selectedGood, setSelectedGood] = useState('Jam');
+  const [selectedGood, setSelectedGood] = useState(goods[8]);
 
   const deleteGood = () => {
     setSelectedGood('');
-  };
+  }
 
   const addGood = good => {
     setSelectedGood(good);
-  };
-
+  }
+ 
   return (
     <main className="section container">
-      <h1 className="title is-flex is-align-items-center">
-        {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
+    <h1 className="title is-flex is-align-items-center">
+      {selectedGood ? `${selectedGood} is selected` : 'No goods selected'}
         <h1 className="title is-flex is-align-items-center">
-          {selectedGood && (
-            <button
-              data-cy="ClearButton"
-              type="button"
-              className="delete ml-3"
-              onClick={deleteGood}
-            />
-          )}
-        </h1>
+        {selectedGood && <button 
+        data-cy="ClearButton" 
+        type="button" 
+        className="delete ml-3"
+        onClick={deleteGood}
+        />}
       </h1>
+    </h1>
 
-      <table className="table">
+    <table className="table">
         <tbody>
           {goods.map(good => {
             const isSelected = good === selectedGood;
-
-            return (
-              <tr
+            
+              return (
+                <tr 
                 key={good}
                 data-cy="Good"
-                className={isSelected ? `has-background-success-light` : ''}
-              >
-                <td>
+                className={classNames({ 'has-background-success-light': isSelected })}
+                >
+                  <td>
                   {isSelected ? (
-                    <button
+                      <button
                       data-cy="RemoveButton"
                       type="button"
                       className="button is-info"
@@ -64,24 +65,26 @@ export const App = () => {
                       -
                     </button>
                   ) : (
-                    <button
-                      data-cy="AddButton"
-                      type="button"
+                    <button 
+                    data-cy="AddButton" 
+                      type="button" 
                       className="button"
                       onClick={() => addGood(good)}
-                    >
-                      +
-                    </button>
+                      >
+                        +
+                      </button>
                   )}
-                </td>
-                <td data-cy="GoodTitle" className="is-vcentered">
-                  {good}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </main>
+                  </td>
+                  <td 
+                  data-cy="GoodTitle" 
+                  className="is-vcentered">
+                    {good}
+                  </td>
+                </tr>
+              )
+          })}  
+      </tbody>
+    </table>
+  </main>
   );
 };
